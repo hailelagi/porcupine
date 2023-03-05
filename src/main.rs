5ebadbbec71e11e257f4ad77d110e74dc3252fc3
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-03-05 22:57:27 +0100

    scope

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+///////////////
+// PORCUPINE //
+///////////////
+
+// todo: explore writing tree like data structures in rust
+// todo: revise Map ds, k-v store theory (hashmaps, search trees etc )
+// https://en.wikipedia.org/wiki/Hash_table
+// todo: avl, red-black tree, B-Tree practice
+// todo: explore Clap api
+
 fn main() {
     println!("Hello, world!");
 }
