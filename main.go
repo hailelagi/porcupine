f77e490d6897364032c88a6a83f5fbbb6aaac178
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-03-18 03:53:54 +0100

    basic

--- main.go
+++ main.go
@@ -18,16 +18,26 @@ func handle(l *Resource, k string, v int) {
 	l.current.RWMutex.RLock()
 
 	// mutate stuff
+	l.current.fields[k] = v
 
 	defer l.current.RUnlock()
 }
 
 func main() {
-
-	shareMe := LockingMap{
+	shareMe := &LockingMap{
 		RWMutex: sync.RWMutex{},
 		fields:  make(map[string]int),
 	}
 
-	fmt.Println(shareMe)
+	x := Resource{current: shareMe}
+	ref := &x
+
+	go func() {
+		handle(ref, "test", 1)
+		handle(ref, "test-x", 2)
+		handle(ref, "test-y", 3)
+		handle(ref, "test-z", 4)
+	}()
+
+	fmt.Println(shareMe.fields)
 }
