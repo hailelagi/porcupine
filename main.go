78cc0cc67de48a961a9dd936caf1bf5066860d19
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-03-18 04:03:59 +0100

    write lock

--- main.go
+++ main.go
@@ -15,12 +15,15 @@ type Resource struct {
 }
 
 func handle(l *Resource, k string, v int) {
-	l.current.RWMutex.RLock()
-
+	// l.current.RWMutex.RLock()
 	// mutate stuff
+	// this can and does panic due to "test"=1 and "test"= 69
+	// accessing the same memory space must use a "write" lock
+
+	l.current.RWMutex.Lock()
 	l.current.fields[k] = v
 
-	defer l.current.RUnlock()
+	defer l.current.Unlock()
 }
 
 func main() {
@@ -32,12 +35,11 @@ func main() {
 	x := Resource{current: shareMe}
 	ref := &x
 
-	go func() {
-		handle(ref, "test", 1)
-		handle(ref, "test-x", 2)
-		handle(ref, "test-y", 3)
-		handle(ref, "test-z", 4)
-	}()
+	go handle(ref, "test", 1)
+	go handle(ref, "test-x", 2)
+	go handle(ref, "test-y", 3)
+	go handle(ref, "test-z", 4)
+	go handle(ref, "test", 69)
 
 	fmt.Println(shareMe.fields)
 }
