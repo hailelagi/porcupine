e9c458b46336c88230ddc44a23e1fb73b3b0b223
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-03-18 03:40:33 +0100

    locking map mutex

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type LockingMap struct {
+	sync.RWMutex
+	fields map[string]int
+}
+
+type Resource struct {
+	current *LockingMap
+}
+
+func handle(l *Resource, k string, v int) {
+	l.current.RWMutex.RLock()
+
+	// mutate stuff
+
+	defer l.current.RUnlock()
+}
 
 func main() {
-	fmt.Println("init")
+
+	shareMe := LockingMap{
+		RWMutex: sync.RWMutex{},
+		fields:  make(map[string]int),
+	}
+
+	fmt.Println(shareMe)
 }
