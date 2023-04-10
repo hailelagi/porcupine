0ca0b0260c26a6c45e3183a4b8ee643f2470f601
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-04-10 02:26:11 +0100

    fix bad interface type cast

--- porcupine/concurrent_append_map.go
+++ porcupine/concurrent_append_map.go
@@ -13,9 +13,10 @@ type ConcurrentAppendMap struct {
 
 func (c *ConcurrentAppendMap) Get(key string) int {
 	value, found := c.Fields.Load(key)
+	v, ok := value.(int)
 
-	if found && value != nil {
-		return value.(int)
+	if found && value != nil && ok {
+		return v
 	} else {
 		return 0
 	}
@@ -23,8 +24,15 @@ func (c *ConcurrentAppendMap) Get(key string) int {
 
 func (c *ConcurrentAppendMap) Put(key string, value int) int {
 	existing, _ := c.Fields.LoadOrStore(key, value)
+	v, ok := existing.(int)
 
-	return existing.(int)
+	if ok {
+		return v
+	} else {
+		// todo: return error
+		return 0
+	}
+
 }
 
 func (c *ConcurrentAppendMap) In(key string) bool {
