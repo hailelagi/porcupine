1dc4559007f53ca8a27a044f5df8cad902e8f622
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-04-29 15:23:06 +0100

    wip

--- api/server.go
+++ api/server.go
@@ -6,13 +6,8 @@ import (
 )
 
 func Serve() {
-	// TODO: PROBLEM UNRESOLVED - HOW TO CONNECT THE CLI TO A LONG RUNNING PROCESS IN-MEMORY
-	// AND PROVIDE RUNTIME INTROSPECTION via a LOG stream of running operations
-
-	// IDEA1: Expose a global porcupine.Store
-	// IDEA2: FIND A WAY TO DO sys IPC CALLS, either by fork() on the current cli process?
-	// IDEA3: use a third party tool to persist in-memory state or write to disk...
-
+	// SPAWN a server process seperate from the CLI process.
+	// Perform IPC calls
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Listening you are on: %s", r.URL.Path)
 	})
