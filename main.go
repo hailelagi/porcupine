068f3a64fd9c44c98cdaa683453aea47d25ebd61
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-12-24 20:22:12 +0100

    ref

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/hailelagi/porcupine-go/cmd"
 
+// maybe refactor to only use flag?
+// https://abhinavg.net/2022/08/13/flag-subcommand/
 func main() {
 	cmd.Execute()
 }
